747a48776f41d572e902f5674c4f87011e7e781b
Samanosukeh <flauberth_94@hotmail.com>
2021-04-28 22:18:52 -0300

    Contexto do Player levado para dentro do PlayerContext.tsx como um componente

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,50 @@
+import { createContext, useState } from 'react';
+
+type Episode = {
+    title: string;
+    members: string;
+    thumbnail: string;
+    duration: number;
+    url: string;
+}
+
+
+type PlayerContextData = {
+    episodeList: Episode[],
+    currentEpisodeIndex: number; //vai apontar o index de qual ep está tocando
+    play: (episode: Episode) => void; //3° argumento é uma função do tipo void
+    isPlaying: boolean;//guarda o estado se um episodio ta tocando
+    togglePlay: () => void;
+    setPlayingState: (state: boolean) => void;
+};
+
+export const PlayerContext = createContext({} as PlayerContextData);
+
+
+export function PlayerContextProvider({ children }) {
+    const [episodeList, setEpisodeList] = useState([]);//variável de estado para alterar um estado no React
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);//true porque quando der play em um episódio ele já sai tocando
+
+  /*função para manipular o estado*/
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);//para que o ep acima seja o que está tocando no momento
+    setIsPlaying(true);//importante para trocar o play para verdadeiro
+  }
+
+  function togglePlay() {//se tiver tocando pausa se tiver pausado toca
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
+  return (
+    //tudo que ta dentro da tag PlayerContext tem acesso ao contexto
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState}}>
+        { children }
+    </PlayerContext.Provider>
+  )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,38 +2,17 @@ import '../styles/global.scss';
 
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
-import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
-import { useState } from 'react';
+import { PlayerContextProvider } from '../contexts/PlayerContext';
 
 /**
  * Esse é o arquivo que fica por volta de toda a aplicação
  */
 
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);//variável de estado para alterar um estado no React
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);//true porque quando der play em um episódio ele já sai tocando
-
-  /*função para manipular o estado*/
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);//para que o ep acima seja o que está tocando no momento
-    setIsPlaying(true);//importante para trocar o play para verdadeiro
-  }
-
-  function togglePlay() {//se tiver tocando pausa se tiver pausado toca
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
   return (
-    //tudo que ta dentro da tag PlayerContext tem acesso ao contexto
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState}}> 
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
         <main>
           <Header />
@@ -41,7 +20,7 @@ function MyApp({ Component, pageProps }) {
         </main>
         <Player />
       </div>
-    </PlayerContext.Provider>
+    </PlayerContextProvider>
   )
 }
 
